1376951c6409c8c3f3226fd0954eb594938e3479
KotMeow <kaamil.kot@gmail.com>
2017-05-27 20:12:58 +0200

    chat look and feel

--- public/javascripts/notification.button.js
+++ public/javascripts/notification.button.js
@@ -5,6 +5,18 @@ $(function () {
     $('.notification-icon').css('color', '#7a7a7a');
     box.toggle('fast');
   });
+  $('.live-chat header').on('click', function() {
 
+    $(this).next().slideToggle(300, 'swing');
+
+  });
+
+  $('.chat-close').on('click', function(e) {
+    console.log($(this).parent().parent());
+    let x = $(this).parent().parent();
+    e.preventDefault();
+    x.fadeOut(300);
+
+  });
 
 });
